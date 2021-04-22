f537e30160988efdc7236e9d75f42cc8e03c7b76
higsch <matthias@higsch.com>
2021-04-22 14:41:38 +0200

    Make histogram pretty

--- hist.js
+++ hist.js
@@ -1,61 +1,189 @@
-var margin = {top: 20, right: 20, bottom: 70, left: 40},
-    width = (screen.width * .75) - margin.left - margin.right,
-    height = (screen.height * .5) - margin.top - margin.bottom;
+var container = document.getElementById('hist');
+var margin = { top: 20, right: 10, bottom: 70, left: 10 },
+  width = container.clientWidth - margin.left - margin.right,
+  height = container.clientHeight - margin.top - margin.bottom;
 
 // Parse the handValue / time
-var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+var x = d3.scale.ordinal().rangeRoundBands([0, width], 0.05);
 
 var y = d3.scale.linear().range([height, 0]);
 
+const xTicks = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26];
 var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom")
-    //.tickFormat(d3.time.format("%Y-%m"));
+  .scale(x)
+  .orient('bottom')
+  .tickSize(0)
+  .tickPadding(7)
+  .tickValues(width < 600 ? xTicks.filter(d => d % 2 === 0) : xTicks);
 
 var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(10);
+  .scale(y)
+  .orient('left')
+  .ticks(10)
+  .tickSize(0)
+  .tickPadding(10);
 
-var svg = d3.select("#hist").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", 
-          "translate(" + margin.left + "," + margin.top + ")");
+var xStarts = [12, 16, 19];
+var gridLineData = [5, 10, 15]
+  .map((d, i) => ({
+    y: d,
+    xStart: xStarts[i]
+  }));
 
-d3.csv("data/bar-data.csv", function(error, data) {
+var svg = d3
+  .select('#hist')
+  .append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-    data.forEach(function(d) {
-        d.handValue = +d.handValue;
-        d.percent = +d.percent;
-    });
-	
-  x.domain(data.map(function(d) { return d.handValue; }));
-  y.domain([0, d3.max(data, function(d) { return d.percent; })]);
+d3.csv('data/bar-data.csv', function (error, data) {
+  data.forEach(function (d) {
+    d.handValue = +d.handValue;
+    d.percent = +d.percent;
+  });
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+  x.domain(
+    data.map(function (d) {
+      return d.handValue;
+    })
+  );
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Games [%]");
+  y.domain([
+    0,
+    d3.max(data, function (d) {
+      return d.percent;
+    }),
+  ]);
 
-  svg.selectAll("bar")
-      .data(data)
-    .enter().append("rect")
-      .style("fill", "#972a27")
-      .attr("x", function(d) { return x(d.handValue); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.percent); })
-      .attr("height", function(d) { return height - y(d.percent); });
+  svg
+    .append('g')
+    .attr('class', 'x axis')
+    .attr('transform', `translate(0 ${height})`)
+    .call(xAxis);
 
+  // defs
+  var defs = svg
+    .append('defs');
+
+  defs
+    .append('marker')
+    .attr('id', 'arrowhead')
+    .attr('markerWidth', 10)
+    .attr('markerHeight', 7)
+    .attr('refX', 0)
+    .attr('refY', 3.5)
+    .attr('orient', 'auto')
+    .append('polygon')
+    .attr('points', '0 0, 10 3.5, 0 7');
+
+  // grid lines
+  svg
+    .append('g')
+    .attr('class', 'y-grid-lines')
+    .selectAll('g')
+    .data(gridLineData)
+    .enter()
+    .append('g')
+    .attr('transform', d => `translate(${x(d.xStart)} ${y(d.y)})`)
+    .append('line')
+    .attr('x2', d => width - x(d.xStart) - margin.right)
+    .attr('y2', 0)
+    .attr('stroke-dasharray', '3, 3');
+
+  // grid line text
+  svg
+    .append('g')
+    .attr('class', 'y-grid-line-text')
+    .selectAll('g')
+    .data(gridLineData)
+    .enter()
+    .append('g')
+    .attr('transform', d => `translate(${x(d.xStart) - 5000 / width} ${y(d.y)})`)
+    .append('text')
+    .attr('dy', -6)
+    .text(d => `${d.y}%`);
+  
+  // vertical win/loose mark
+  svg
+    .append('g')
+    .attr('class', 'win-lose-mark')
+    .selectAll('g')
+    .data([22])
+    .enter()
+    .append('g')
+    .attr('transform', d => `translate(${x(d)} 0)`)
+    .append('line')
+    .attr('x2', 0)
+    .attr('y2', height)
+    .attr('stroke-dasharray', '3, 3');
+
+  // labels
+  var xLabel = svg
+    .append('g')
+    .attr('class', 'x-label')
+    .attr('transform', `translate(${x(8)} ${height + margin.bottom / 1.5})`);
+
+  var xLabelText = xLabel
+    .append('text')
+    .text('Hand value at the end of the game');
+
+  if (width > 600) {
+    xLabel
+      .append('path')
+      .attr('d', `m${xLabelText.node().getBBox().width + 5} -3q20 0 25 -10`)
+      .attr('marker-end', 'url(#arrowhead)');
+  }
+
+  var yLabel = svg
+    .append('g')
+    .attr('class', 'y-label')
+    .attr('transform', `translate(${x(13) - (width < 600 ? 40 : 0)} ${height / 5})`);
+
+  yLabel
+    .append('text')
+    .text('Fraction of all games')
+    .attr('dx', - 10000 / width);
+
+  yLabel
+    .append('path')
+    .attr('d', `M${x(10) - 7} 7q5 20 20 22`)
+    .attr('marker-end', 'url(#arrowhead)');
+
+  var labelPlayerLoses = svg
+    .append('g')
+    .attr('class', 'label-player-loses')
+    .attr('transform', `translate(${x(22) + 3} 5)`);
+
+  labelPlayerLoses
+    .append('text')
+    .attr('dx', 25)
+    .attr('dy', 5)
+    .text('Player loses');
+
+  labelPlayerLoses
+    .append('path')
+    .attr('d', 'M0 0L10 0')
+    .attr('marker-end', 'url(#arrowhead)');
+
+  svg
+    .selectAll('bar')
+    .data(data)
+    .enter()
+    .append('rect')
+    .style('fill', '#0a509c')
+    .style('opacity', 0.9)
+    .attr('x', function (d) {
+      return x(d.handValue);
+    })
+    .attr('width', x.rangeBand())
+    .attr('y', function (d) {
+      return y(d.percent);
+    })
+    .attr('height', function (d) {
+      return height - y(d.percent);
+    })
+    .attr('rx', 5)
+    .attr('ry', 5);
 });
--- main.js
+++ main.js
@@ -1,21 +1,19 @@
-const suits = ['S', 'H', 'D', 'C'];
-const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-const deck = new Array();
-const players = new Array();
-
-let agreement_data, winProbs, dealers;
+var suits = ["S", "H", "D", "C"];
+var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+var deck = new Array();
+var players = new Array();
 
 // load data set
-d3.csv('data/hand_value_averages.csv', function (data) {
-  data.forEach(function (d) {
+d3.csv("data/hand_value_averages.csv", function (data) {
+  data.forEach(function (d, i) {
     d.hand_value = +d.hand_value;
     d.tookCard = (+d.tookCard * 100).toFixed(0);
   });
   agreement_data = data;
 });
 
-d3.csv('data/winProbs.csv', function (data) {
-  data.forEach(function (d) {
+d3.csv("data/winProbs.csv", function (data) {
+  data.forEach(function (d, i) {
     d.handValue = +d.handValue;
   });
   winProbs = data;
@@ -26,8 +24,8 @@ function createDeck() {
   for (var i = 0; i < values.length; i++) {
     for (var x = 0; x < suits.length; x++) {
       var weight = parseInt(values[i]);
-      if (values[i] == 'J' || values[i] == 'Q' || values[i] == 'K') weight = 10;
-      if (values[i] == 'A') weight = 11;
+      if (values[i] == "J" || values[i] == "Q" || values[i] == "K") weight = 10;
+      if (values[i] == "A") weight = 11;
       var card = { Value: values[i], Suit: suits[x], Weight: weight };
       deck.push(card);
     }
@@ -37,63 +35,63 @@ function createDeck() {
 function createPlayer() {
   players = new Array();
   var hand = new Array();
-  var player = { Name: 'Player', ID: 1, Points: 0, Hand: hand, hasAce: 0 };
+  var player = { Name: "Player", ID: 1, Points: 0, Hand: hand, hasAce: 0 };
   players.push(player);
 }
 
 function createDealer() {
   dealers = new Array();
   var hand = new Array();
-  var dealer = { Name: 'Dealer', ID: 1, Points: 0, Hand: hand, hasAce: 0 };
+  var dealer = { Name: "Dealer", ID: 1, Points: 0, Hand: hand, hasAce: 0 };
   dealers.push(dealer);
 }
 
 function createPlayersUI() {
-  document.getElementById('players').innerHTML = '';
+  document.getElementById("players").innerHTML = "";
 
-  var div_player = document.createElement('div');
+  var div_player = document.createElement("div");
 
-  var div_points = document.createElement('div');
-  var div_hand = document.createElement('div');
+  var div_points = document.createElement("div");
+  var div_hand = document.createElement("div");
 
-  div_points.className = 'points';
-  div_points.id = 'points_0';
-  div_player.id = 'player_0';
-  div_player.className = 'player';
-  div_hand.id = 'hand_0';
+  div_points.className = "points";
+  div_points.id = "points_0";
+  div_player.id = "player_0";
+  div_player.className = "player";
+  div_hand.id = "hand_0";
 
-  var h = document.createElement('h1');
-  var t = document.createTextNode('Player');
+  var h = document.createElement("h1");
+  var t = document.createTextNode("Player");
   h.appendChild(t);
   div_player.appendChild(h);
 
   div_player.appendChild(div_points);
   div_player.appendChild(div_hand);
-  document.getElementById('players').appendChild(div_player);
+  document.getElementById("players").appendChild(div_player);
 }
 
 function createDealersUI() {
-  document.getElementById('dealers').innerHTML = '';
+  document.getElementById("dealers").innerHTML = "";
 
-  var div_dealer = document.createElement('div');
+  var div_dealer = document.createElement("div");
 
-  var div_points_dealer = document.createElement('div');
-  var div_hand_dealer = document.createElement('div');
+  var div_points_dealer = document.createElement("div");
+  var div_hand_dealer = document.createElement("div");
 
-  div_points_dealer.className = 'points';
-  div_points_dealer.id = 'points_dealer_0';
-  div_dealer.id = 'dealer_0';
-  div_dealer.className = 'player';
-  div_hand_dealer.id = 'hand_dealer_0';
+  div_points_dealer.className = "points";
+  div_points_dealer.id = "points_dealer_0";
+  div_dealer.id = "dealer_0";
+  div_dealer.className = "player";
+  div_hand_dealer.id = "hand_dealer_0";
 
-  var h = document.createElement('h1');
-  var t = document.createTextNode('Dealer');
+  var h = document.createElement("h1");
+  var t = document.createTextNode("Dealer");
   h.appendChild(t);
   div_dealer.appendChild(h);
 
   div_dealer.appendChild(div_points_dealer);
   div_dealer.appendChild(div_hand_dealer);
-  document.getElementById('dealers').appendChild(div_dealer);
+  document.getElementById("dealers").appendChild(div_dealer);
 }
 
 function shuffle() {
@@ -110,18 +108,18 @@ function shuffle() {
 }
 
 function startblackjack() {
-  document.getElementById('gameResult').innerHTML = '';
-  document.getElementById('movePercent').innerHTML = '';
+  document.getElementById("gameResult").innerHTML = "";
+  document.getElementById("movePercent").innerHTML = "";
 
-  document.getElementById('btnStart').value = 'RESTART';
-  document.getElementById('hitButton').disabled = false;
-  document.getElementById('standButton').disabled = false;
+  document.getElementById("btnStart").value = "RESTART";
+  document.getElementById("hitButton").disabled = false;
+  document.getElementById("standButton").disabled = false;
 
-  document.getElementById('btnStart').style.display = 'none';
-  document.getElementById('hitButton').style.display = 'inline-block';
-  document.getElementById('standButton').style.display = 'inline-block';
+  document.getElementById("btnStart").style.display = "none";
+  document.getElementById("hitButton").style.display = "inline-block";
+  document.getElementById("standButton").style.display = "inline-block";
 
-  document.getElementById('winPercentContainer').style.display = 'inline-block';
+  document.getElementById("winPercentContainer").style.display = "inline-block";
 
   // deal 2 cards to every player object
   createDeck();
@@ -131,7 +129,7 @@ function startblackjack() {
   createPlayersUI();
   createDealersUI();
   dealHands();
-  document.getElementById('player_0').classList.add('active');
+  document.getElementById("player_0").classList.add("active");
 
   check();
 }
@@ -141,19 +139,19 @@ function dealHands() {
   // 2 cards each
   for (var i = 0; i < 2; i++) {
     var card = deck.pop();
-    if (card.Value == 'A') {
+    if (card.Value == "A") {
       players[0].hasAce = 1;
     }
     players[0].Hand.push(card);
-    renderCard(card, 'player_0');
+    renderCard(card, "player_0");
     updatePoints();
 
     var card = deck.pop();
-    if (card.Value == 'A') {
+    if (card.Value == "A") {
       dealers[0].hasAce = 1;
     }
     dealers[0].Hand.push(card);
-    renderCard(card, 'dealer_0');
+    renderCard(card, "dealer_0");
     updatePoints();
   }
 }
@@ -164,11 +162,11 @@ function renderCard(card, hand_id) {
 }
 
 function getCardUI(card) {
-  var el = document.createElement('img');
+  var el = document.createElement("img");
 
-  el.src = 'card_imgs/' + card.Value + card.Suit + '.png';
+  el.src = "card_imgs/" + card.Value + card.Suit + ".png";
 
-  el.className = 'card';
+  el.className = "card";
   //el.innerHTML = card.Value + '<br/>' + icon;
   return el;
 }
@@ -204,19 +202,19 @@ function getPoints() {
 
 function updatePoints() {
   getPoints();
-  document.getElementById('points_0').innerHTML = players[0].Points;
-  document.getElementById('points_dealer_0').innerHTML = dealers[0].Points;
+  document.getElementById("points_0").innerHTML = players[0].Points;
+  document.getElementById("points_dealer_0").innerHTML = dealers[0].Points;
 }
 
 function dealerDraw() {
   // pop a card from the deck to the current player
   // check if current player new points are over 21
   var card = deck.pop();
-  if (card.Value == 'A') {
+  if (card.Value == "A") {
     dealers[0].hasAce = 1;
   }
   dealers[0].Hand.push(card);
-  renderCard(card, 'dealer_0');
+  renderCard(card, "dealer_0");
   updatePoints();
   check();
 }
@@ -225,17 +223,17 @@ function hitMe() {
   var alsoHit = agreement_data.filter(function (d) {
     return d.hand_value == players[0].Points;
   })[0].tookCard;
-  document.getElementById('movePercent').innerHTML =
-    alsoHit + '% of players also would have taken another card';
+  document.getElementById("movePercent").innerHTML =
+    alsoHit + "% of players also would have taken another card";
 
   // pop a card from the deck to the current player
   // check if current player new points are over 21
   var card = deck.pop();
-  if (card.Value == 'A') {
+  if (card.Value == "A") {
     players[0].hasAce = 1;
   }
   players[0].Hand.push(card);
-  renderCard(card, 'player_0');
+  renderCard(card, "player_0");
 
   if (dealers[0].Points < 17) {
     dealerDraw();
@@ -251,14 +249,14 @@ function stay() {
     return d.hand_value == players[0].Points;
   })[0].tookCard;
   var alsoStood = 100 - Number(wouldaHit);
-  document.getElementById('movePercent').innerHTML =
-    alsoStood + '% of players also would have stayed';
+  document.getElementById("movePercent").innerHTML =
+    alsoStood + "% of players also would have stayed";
 
-  document.getElementById('hitButton').disabled = true;
-  document.getElementById('standButton').disabled = true;
+  document.getElementById("hitButton").disabled = true;
+  document.getElementById("standButton").disabled = true;
 
-  document.getElementById('player_0').classList.remove('active');
-  document.getElementById('player_0').classList.add('active');
+  document.getElementById("player_0").classList.remove("active");
+  document.getElementById("player_0").classList.add("active");
 
   while (dealers[0].Points < 17) {
     dealerDraw();
@@ -278,66 +276,66 @@ function updateWinPercent(gameOver = 0, winProb) {
     // need to get round
     // round = number of cards player is holding - 1 (since round 1 starts with 2 cards)
     var numCurrentCards = document
-      .getElementById('player_0')
-      .getElementsByTagName('img').length;
+      .getElementById("player_0")
+      .getElementsByTagName("img").length;
 
     // need to cross reference with winProbs dataset
     var winProb = (
-      someProbs['round' + (numCurrentCards - 1).toString()] * 100
+      someProbs["round" + (numCurrentCards - 1).toString()] * 100
     ).toFixed(0);
   }
 
   // update frontend
-  document.getElementById('winPer').innerHTML =
-    '<span style='float:left; margin-left:10px;'>Likelihood Of Player Win</span>' +
+  document.getElementById("winPer").innerHTML =
+    "<span style='float:left; margin-left:10px;'>Likelihood Of Player Win</span>" +
     winProb.toString() +
-    '%';
-  document.getElementById('winPer').style.width = winProb.toString() + '%';
+    "%";
+  document.getElementById("winPer").style.width = winProb.toString() + "%";
 
   return;
 }
 
 function end() {
-  document.getElementById('btnStart').style.display = 'inline-block';
-  document.getElementById('hitButton').style.display = 'none';
-  document.getElementById('standButton').style.display = 'none';
+  document.getElementById("btnStart").style.display = "inline-block";
+  document.getElementById("hitButton").style.display = "none";
+  document.getElementById("standButton").style.display = "none";
 
   getPoints();
 
   if (players[0].Points == 21) {
-    document.getElementById('gameResult').innerHTML = 'Blackjack!';
+    document.getElementById("gameResult").innerHTML = "Blackjack!";
   } else if (players[0].Points > 21) {
-    document.getElementById('gameResult').innerHTML = 'Bust!';
+    document.getElementById("gameResult").innerHTML = "Bust!";
   } else if (dealers[0].Points > 21) {
-    document.getElementById('gameResult').innerHTML = 'Dealer bust!';
+    document.getElementById("gameResult").innerHTML = "Dealer bust!";
   } else if (dealers[0].Points > players[0].Points) {
-    document.getElementById('gameResult').innerHTML = 'Dealer wins!';
+    document.getElementById("gameResult").innerHTML = "Dealer wins!";
   } else if (dealers[0].Points < players[0].Points) {
-    document.getElementById('gameResult').innerHTML = 'You win!';
+    document.getElementById("gameResult").innerHTML = "You win!";
   } else if (dealers[0].Points == players[0].Points) {
-    document.getElementById('gameResult').innerHTML = 'Push!';
+    document.getElementById("gameResult").innerHTML = "Push!";
   }
 }
 
 function check() {
   if (players[0].Points > 21) {
     end();
-    updateWinPercent(1, '0');
+    updateWinPercent(1, "0");
   } else if (players[0].Points == 21) {
     end();
-    updateWinPercent(1, '100');
+    updateWinPercent(1, "100");
   } else if (dealers[0].Points > 21) {
     end();
-    updateWinPercent(1, '100');
+    updateWinPercent(1, "100");
   } else if (dealers[0].Points == 21) {
     end();
-    updateWinPercent(1, '0');
+    updateWinPercent(1, "0");
   } else {
     updateWinPercent();
   }
 }
 
-window.addEventListener('load', function () {
+window.addEventListener("load", function () {
   createDeck();
   shuffle();
   createPlayer();
