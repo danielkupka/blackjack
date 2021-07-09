b67fd11ad3f5180ed0c3ccd3f50f207d28548c46
Loris M <loris.mattioni@gmail.com>
2021-07-09 12:04:01 +0200

    creating a frame page, frame.html + adding the embed game button + adding the embed.js script

---
+++ embed.js
@@ -0,0 +1,17 @@
+function embed() {
+	let x = `
+		<iframe src="frame.html" style="height:800px;width:800px;border:0px;" title="Embeded Game"></iframe>
+	`
+	copyElementText(x);
+	const embedId = document.getElementById("embedding");
+	embedId.classList.remove("hidden");
+}
+
+function copyElementText(txt) {
+    const elem = document.createElement("textarea");
+    document.body.appendChild(elem);
+    elem.value = txt;
+    elem.select();
+    document.execCommand("copy");
+    document.body.removeChild(elem);
+}
